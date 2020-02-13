a6f505be25282a002a46d9fbf0f3d84345b0fb48
liaveras <liaveras13@gmail.com>
2020-02-13 19:26:45 -0300

    Atualização de acordo com a criação da classe Repositorio

--- src/banco/BancoMain.java
+++ src/banco/BancoMain.java
@@ -4,41 +4,63 @@ public class BancoMain {
 
 	public static void main(String[] args) {
 		
-		Cliente cl1 = new Cliente ("Lia Veras", "111.111.111-11");
-		Conta c1 = new Conta("123", cl1);
 		
-		Cliente cl2 = new Cliente ("Rita Veras", "222.222.222-22");
-		Conta c2 = new Conta("456", 0, cl2);
+		RepositorioClientesArray repo = new RepositorioClientesArray();
+		
+		Cliente c1 = new Cliente("Rafael", "123.123.123-12", TipoCliente.VIP);
+		Cliente c2 = new Cliente("Lia", "123.123.123-13", TipoCliente.ESPECIAL);
+		
+		repo.inserir(c1);
+		repo.inserir(c2);
+		
+		repo.status();
 		
 		
 		
-		System.out.println("Conta: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
-		System.out.println("Conta: " + c2.getNumero() + " Saldo: " + c2.getSaldo());
-		System.out.println("-----------------------------------------------------");
 		
-		c1.creditar(550.3);
-		c2.creditar(350.9);
 		
-		System.out.println("-----------------------------------------------------");
-		System.out.println("Conta: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
-		System.out.println("Conta: " + c2.getNumero() + " Saldo: " + c2.getSaldo());
-		System.out.println("-----------------------------------------------------");
 		
-		c1.debitar(50);
-		c2.debitar(100);
 		
-		System.out.println("-----------------------------------------------------");
-		System.out.println("Conta: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
-		System.out.println("Conta: " + c2.getNumero() + " Saldo: " + c2.getSaldo());
-		System.out.println("-----------------------------------------------------");
 		
-		c1.transferir(c2, 700);
 		
-		System.out.println("-----------------------------------------------------");
-		System.out.println("Conta: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
-		System.out.println("Conta: " + c2.getNumero() + " Saldo: " + c2.getSaldo());
-		System.out.println("-----------------------------------------------------");
 		
+		
+//		Exercício 1:
+//		Cliente cl1 = new Cliente ("Lia Veras", "111.111.111-11");
+//		Conta c1 = new Conta("123", cl1);
+//		
+//		Cliente cl2 = new Cliente ("Rita Veras", "222.222.222-22");
+//		Conta c2 = new Conta("456", 0, cl2);
+//		
+//		
+//		
+//		System.out.println("Conta: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
+//		System.out.println("Conta: " + c2.getNumero() + " Saldo: " + c2.getSaldo());
+//		System.out.println("-----------------------------------------------------");
+//		
+//		c1.creditar(550.3);
+//		c2.creditar(350.9);
+//		
+//		System.out.println("-----------------------------------------------------");
+//		System.out.println("Conta: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
+//		System.out.println("Conta: " + c2.getNumero() + " Saldo: " + c2.getSaldo());
+//		System.out.println("-----------------------------------------------------");
+//		
+//		c1.debitar(50);
+//		c2.debitar(100);
+//		
+//		System.out.println("-----------------------------------------------------");
+//		System.out.println("Conta: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
+//		System.out.println("Conta: " + c2.getNumero() + " Saldo: " + c2.getSaldo());
+//		System.out.println("-----------------------------------------------------");
+//		
+//		c1.transferir(c2, 700);
+//		
+//		System.out.println("-----------------------------------------------------");
+//		System.out.println("Conta: " + c1.getNumero() + " Saldo: " + c1.getSaldo());
+//		System.out.println("Conta: " + c2.getNumero() + " Saldo: " + c2.getSaldo());
+//		System.out.println("-----------------------------------------------------");
+//		
 
 		
 			
--- src/banco/Cliente.java
+++ src/banco/Cliente.java
@@ -3,10 +3,12 @@ package banco;
 public class Cliente {
 	private String nome;
 	private String cpf;
+	private TipoCliente tipo;
 	
-	public Cliente(String nome, String cpf) {
+	public Cliente(String nome, String cpf, TipoCliente tipo) {
 		this.nome = nome;
 		this.cpf = cpf;
+		this.tipo = tipo;
 	}
 	
 	public String getNome() {
@@ -24,5 +26,20 @@ public class Cliente {
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+	
+	public TipoCliente getTipo() {
+		return this.tipo;
+	}
+	
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nome=" + nome + ", cpf=" + cpf + ", tipo=" + tipo + "]";
+	}
+	
+	
 	
 }
